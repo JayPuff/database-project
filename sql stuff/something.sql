50ca828bfb2c8af86d7a3fab68be6aab76bd3563
Nick <nicolas.z.correa@gmail.com>
2017-11-24 17:39:10 -0500

    Some table stuff

--- sql stuff/something.sql
+++ sql stuff/something.sql
@@ -27,11 +27,49 @@ CREATE TABLE City (
 
 INSERT INTO Province VALUES('Quebec');
 INSERT INTO Province VALUES('Ontario');
+INSERT INTO Province(province_name)
+VALUES( 'Nova Scotia' ),( 'New Brunswick' ),( 'Manitoba' ),
+('British Columbia'),
+('Prince Edward Island'),
+('Saskatchewan'),
+('Alberta'),
+('Newfoundland and Labrador'),
+('Northwest Territories'),
+('Yukon'),
+('Nunavut');
 
 
 INSERT INTO City VALUES('Montreal','Quebec');
 INSERT INTO City VALUES('Quebec City','Quebec');
 INSERT INTO City VALUES('Toronto','Ontario');
+
+INSERT INTO City(city_name, province_name)
+VALUES('Victoria','British Columbia'),
+('Kelowna','British Columbia'),
+('Moncton','New Brunswick'),
+('Saint John','New Brunswick'),
+('Fredericton','New Brunswick'),
+('Labrador City','Newfoundland and Labrador'),
+('Corner Brook','Newfoundland and Labrador'),
+('Gander','Newfoundland and Labrador'),
+('Fort Simpson','Northwest Territories'),
+('Yellowknife','Northwest Territories'),
+('Inuvik','Northwest Territories'),
+('Halifax','Nova Scotia'),
+('Sydney','Nova Scotia'),
+('Truro','Nova Scotia'),
+('Iqaluit','Nunavut'),
+('Baker Lane','Nunavut'),
+('Rankin Inlet','Nunavut'),
+('Charlottetown','Prince Edward Island'),
+('Summerside','Prince Edward Island'),
+('Montague','Prince Edward Island'),
+('Regina','Saskatchewan'),
+('Saskatoon','Saskatchewan'),
+('Moose Jaw','Saskatchewan'),
+('Whitehorse','Yukon'),
+('Dawson City','Yukon'),
+('Watson Lake','Yukon');
 
 
 
