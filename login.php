6b9f0ea716df0bd91b8463da0af5969da535dda2
Nick <nicolas.z.correa@gmail.com>
2017-11-24 17:33:20 -0500

    Login page and register page are complete and tested!

--- login.php
+++ login.php
@@ -6,71 +6,85 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="index.css"/>
-    <title>  Login  </title>
+    <title>  Register  </title>
   </head>
   <body>
     <?php
-      $showform = true;
-      $registered = false;
+    $showform = true;
 
-      if(isset($_POST["username"]) && isset($_POST["pass"])) {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+    if(isset($_POST["username"]) && isset($_POST["pass"])) {
+         
         try {
-          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-          // set the PDO error mode to exception
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
-      
-          $stmt = $conn->prepare("INSERT INTO User (Username,Pass,Firstname,Lastname,Membership,Province,City) VALUES (:username, :pass, :firstname, :lastname, :membership, :province, :city)");
+            
+          $stmt = $conn->prepare("SELECT * FROM User WHERE Username= :username AND Pass= :pass");
           
-          $nick = "nick";
-          $number = 0;
           // Prepare statement
           $stmt->bindParam(':username', $_POST["username"]);
           $stmt->bindParam(':pass', $_POST["pass"]);
-          $stmt->bindParam(':firstname', $nick);
-          $stmt->bindParam(':lastname', $nick);
-          $stmt->bindParam(':membership', $number);
-          $stmt->bindParam(':province', $nick);
-          $stmt->bindParam(':city', $nick);
 
           
           
           // execute the query
-          $result = $stmt->execute();
-      
-          if($result) {
-            $showform = false;
-            $registered = true;
-            echo "<h1> Registered successfully! </h1>";
+          $stmt->execute();
+
+          
+
+          $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+          $results = $stmt->fetchAll();
+          
+          $count = 0;
+          for($i = 0; $i < count($results); $i++) {
+            $count = 1;
+         }
+
+          if($count==0)
+          {
+            echo "<h1> Error logging in, please try again! </h1>";
           }
+          else
+          {
+            $showform = false;
+            echo "<h1> Logged in successfully! </h1>";
+          }
+          
       
       } catch (PDOException $e) {
-          // $error =  array('error' => $e->getMessage());
-          // echo json_encode($error);
+           $error =  array('error' => $e->getMessage());
+           echo json_encode($error);
+
       }
       
       
       $conn = null;
-      return;
     }
+
+    if($showform) {
+        echo "<form action='login.php' method='post'>
+                <h2> Login Form </h2>
+                <div> Username: <input required name='username' type='text'/> </div> <br>
+                <div> Password: <input required name='pass' type='password'> </div> <br>";
+        
+        
+
+        echo "<button type='submit' > LOGIN </button>
+            </form>";
+    }
+
     ?>
-
-
-    <?  if($showform) { 
-
-     echo "<form action='login.php' method='post'>
-        <input name='username' type='text'/>
-        <input name='pass' type='password'>
-        <button type='submit'> REGISTER </button>
-          </form>";
-     } ?>  
 
 
 
 
 
     
-    <!-- <script type="text/javascript" src="libs/jquery/jquery-3.2.1.min.js"></script> -->
+    <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
     <script src="index.js" type="text/javascript"> </script>
   </body>
 </html>
